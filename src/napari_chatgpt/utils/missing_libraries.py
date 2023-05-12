454464c323caf32fbd9e3eaea5042cab3272f83c
hinderling <lucien.hinderling@gmail.com>
2023-05-12 21:42:33 +0200

    parse PyPI for package names instead of just infering from library import

--- src/napari_chatgpt/utils/missing_libraries.py
+++ src/napari_chatgpt/utils/missing_libraries.py
@@ -1,6 +1,9 @@
 import subprocess
 import sys
 import traceback
+import difflib
+import re
+from bs4 import BeautifulSoup
 from functools import cache
 from subprocess import CalledProcessError
 from typing import List
@@ -10,6 +13,7 @@ from langchain import LLMChain, PromptTemplate
 from langchain.callbacks import StdOutCallbackHandler, CallbackManager
 from langchain.chat_models import ChatOpenAI
 from langchain.llms import BaseLLM
+import requests
 
 from napari_chatgpt.utils.openai_key import set_openai_key
 
@@ -67,19 +71,52 @@ def required_libraries(code: str, llm: BaseLLM = None):
     return list_of_packages
 
 
+def validate_package_name(package_name):
+    """Find the most relevant package name for a given package name using PyPI search.
+    This helps to work around cases where the library name is not exactly the same as the PyPI package name, e.g. useq and useq-schema."""
+    
+    search_url = f"https://pypi.org/search/?q={package_name}"
+    response = requests.get(search_url)
+    if response.status_code == 200:
+
+        #get all links from the searched page
+        soup = BeautifulSoup(response.content, "html.parser")
+        package_list = [link.get("href") for link in soup.find_all("a",href=True)]
+
+        #find links that match the format /project/<any_package>/ and return [<any_package_name>]
+        regex = re.compile(r".*/project/(.*)")
+        matches = [regex.match(string).group(1)[:-1] for string in package_list if regex.match(string)]
+
+        #find the closest match to the package name
+        closest_match = difflib.get_close_matches(package_name, matches, n=1,cutoff=0)
+        print(closest_match)
+        if closest_match ==[]:
+            return None
+        else:
+            return closest_match[0]
+
+    else:
+        #Could not access PyPI search
+        print(f"Could not find package name for '{package_name}'")
+        return None
+
+
+
 def pip_install(packages: List[str], ignore_obvious: bool = True) -> bool:
     if ignore_obvious:
         packages = [p for p in packages if
-                    not p in ['numpy', 'napari', 'magicgui', 'scikit-image']]
+                    not p in ['numpy', 'napari', 'magicgui', 'scikit-image','useq-schema']]
 
     try:
         with asection(f"Installing {len(packages)} packages with pip:"):
             for package in packages:
+                package = validate_package_name(package)
                 _pip_install_single_package(package)
         return True
     except CalledProcessError:
         aprint(traceback.format_exc())
         return False
+
 
 
 @cache
